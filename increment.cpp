fb3df68322e508719b0b3cc78726dcc1d69d92e4
Jashwant kumar shah <shahjashwant9119@gmail.com>
2026-08-18 07:19:52 -0700

    Add files via upload

---
+++ increment.cpp
@@ -0,0 +1,22 @@
+// post and pre increment and decrement 
+// x++ , ++x , x-- , --x
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 10;
+    cout<<x<<endl;
+    x++; // x = x + 1
+    cout<<x<<endl;
+}
+
+//  also we used decrement 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 10;
+    cout<<x<<endl;
+    x--; // x = x + 1
+    cout<<x<<endl;
+}
